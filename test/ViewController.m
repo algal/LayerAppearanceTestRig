4dbebef04e7202feb953bed16d10074c26ae24e0
Alexis Gallagher <alexis@alexisgallagher.com>
2012-11-12 15:58:32 +0000

    web version

--- test/ViewController.m
+++ test/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -18,6 +19,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  
+  CGRect imgFrame = CGRectMake(100, 100, 100, 100);
+  UIView *imgView = [[UIView alloc] initWithFrame:imgFrame];
+  imgView.backgroundColor = [UIColor clearColor];
+  UIImage *image = [UIImage imageNamed:@"mandel_images.png"];
+  imgView.layer.backgroundColor = [UIColor colorWithPatternImage:image].CGColor;
+  
+  // Rounded corners.
+  imgView.layer.cornerRadius = 10;
+  
+  // A thin border.
+  imgView.layer.borderColor = [UIColor blackColor].CGColor;
+  imgView.layer.borderWidth = 0.3;
+  
+  // Drop shadow.
+  imgView.layer.shadowColor = [UIColor blackColor].CGColor;
+  imgView.layer.shadowOpacity = 1.0;
+  imgView.layer.shadowRadius = 7.0;
+  imgView.layer.shadowOffset = CGSizeMake(0, 4);
+  
+  [self.view addSubview:imgView];
 }
 
 - (void)didReceiveMemoryWarning
