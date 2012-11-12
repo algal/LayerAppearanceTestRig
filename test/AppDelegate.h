949385bd1d59a6afd2f2b7c47e9f38f3e2993bb9
Alexis Gallagher <alexis@alexisgallagher.com>
2012-11-12 15:53:35 +0000

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Alexis Gallagher on 2012-11-12.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Alexis Gallagher on 2012-11-12.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
